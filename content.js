1399ca0a02fbec7bd49629e2479bad25ffe03f60
nitro56565 <nitro56565@gmail.com>
2024-04-04 02:20:20 +0530

    button css done

--- content.js
+++ content.js
@@ -2,8 +2,20 @@ function addButton() {
   const toolbarDiv = document.querySelector('div[data-testid="toolBar"]');
   if (toolbarDiv && !toolbarDiv.hasAttribute('data-button-added')) {
     
-    const button = document.createElement('button');
-    button.textContent = 'Mahesh Tweets';
+    const button = document.createElement('img');
+    button.src = 'https://i.ibb.co/CP1gchy/icon.png';
+    button.classList.add('fetch-button');
+    button.style.backgroundColor = 'yellow';
+    button.style.width = '36px';
+    button.style.height = '36px';
+    button.style.border = 'none';
+    button.style.cursor = 'pointer';
+    button.style.borderRadius = '50%';
+    button.style.position = 'relative'
+    button.style.left = '32rem'
+
+    // img.src = '/images/icon.png';
+    // button.appendChild(img);
 
     if (toolbarDiv.parentNode) {
       toolbarDiv.parentNode.insertBefore(button, toolbarDiv);
