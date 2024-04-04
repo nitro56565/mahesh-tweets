a738dbb89214c2c0098a59a58227c84ac366739a
nitro56565 <nitro56565@gmail.com>
2024-04-05 00:58:15 +0530

    alert for active tweet added

--- content.js
+++ content.js
@@ -1,7 +1,7 @@
 function addButton() {
   const toolbarDiv = document.querySelector('div[data-testid="toolBar"]');
   if (toolbarDiv && !toolbarDiv.hasAttribute('data-button-added')) {
-    
+
     const button = document.createElement('img');
     button.src = 'https://i.ibb.co/CP1gchy/icon.png';
     button.classList.add('fetch-button');
@@ -11,19 +11,29 @@ function addButton() {
     button.style.border = 'none';
     button.style.cursor = 'pointer';
     button.style.borderRadius = '50%';
-    button.style.position = 'relative'
-    button.style.left = '32rem'
+    button.style.position = 'relative';
+    button.style.left = '32rem';
 
-    // img.src = '/images/icon.png';
-    // button.appendChild(img);
+    button.addEventListener('click', function() {
+      const replySections = document.querySelectorAll('article[role="article"][tabindex="-1"]');
+      replySections.forEach(section => {
+        const tweetTextElement = section.querySelector('div[data-testid="tweetText"] > span');
+        if (tweetTextElement) {
+          const tweetText = tweetTextElement.innerText;
+          if (tweetText) {
+            alert(tweetText);
+          }
+        }
+      });
+    });
 
     if (toolbarDiv.parentNode) {
       toolbarDiv.parentNode.insertBefore(button, toolbarDiv);
       console.log('Selected toolbar div:', toolbarDiv);
-      console.log("Button Added",button);
+      console.log("Button Added", button);
     }
     toolbarDiv.setAttribute('data-button-added', 'true');
   }
-  setTimeout(addButton, 200); //delay added
+  setTimeout(addButton, 200); // delay added
 }
 addButton();
