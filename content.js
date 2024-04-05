de17e36d39387e320f1e051b1fb673f1217221e3
nitro56565 <nitro56565@gmail.com>
2024-04-05 13:38:54 +0530

    tweet paste implemented

--- content.js
+++ content.js
@@ -1,28 +1,28 @@
 function addButton() {
   const toolbarDiv = document.querySelector('div[data-testid="toolBar"]');
   if (toolbarDiv && !toolbarDiv.hasAttribute('data-button-added')) {
-
     const button = document.createElement('img');
     button.src = 'https://i.ibb.co/CP1gchy/icon.png';
     button.classList.add('fetch-button');
     button.style.backgroundColor = 'yellow';
-    button.style.width = '36px';
-    button.style.height = '36px';
+    button.style.width = '32px';
+    button.style.height = '32px';
     button.style.border = 'none';
     button.style.cursor = 'pointer';
     button.style.borderRadius = '50%';
     button.style.position = 'relative';
-    button.style.left = '32rem';
+    button.style.left = '32.5rem';
 
-    button.addEventListener('click', function() {
-      const replySections = document.querySelectorAll('article[role="article"][tabindex="-1"]');
-      replySections.forEach(section => {
-        const tweetTextElement = section.querySelector('div[data-testid="tweetText"] > span');
+    button.addEventListener('click', () => {
+      const tweetSections = document.querySelectorAll('article[role="article"][tabindex="-1"]');
+      tweetSections.forEach(section => {
+        const tweetTextElement = section.querySelector('div[data-testid="tweetText"]');
         if (tweetTextElement) {
-          const tweetText = tweetTextElement.innerText;
-          if (tweetText) {
-            alert(tweetText);
-          }
+          const tweetText = tweetTextElement.textContent.trim();
+            if (tweetText) {
+              alert(tweetText);
+            }
+          pasteMessage(tweetText); 
         }
       });
     });
@@ -36,4 +36,19 @@ function addButton() {
   }
   setTimeout(addButton, 200); // delay added
 }
+
+function pasteMessage(text) {
+  const replyTextarea = document.querySelector('div[aria-label="Post text"]')
+  console.log('Reply textarea:', replyTextarea);
+
+  if (replyTextarea) {
+    replyTextarea.focus();
+    document.execCommand('insertText', false, text);
+    console.log('Text inserted:', text);
+  } else {
+    console.error("Reply textarea not found");
+  }
+}
+
+
 addButton();
